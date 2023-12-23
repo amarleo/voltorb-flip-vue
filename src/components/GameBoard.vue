49d7d92c82944005f05df9a6ea05627b2e7bab77
amarleo <alu0101015941@ull.edu.es>
2023-12-23 17:48:22 +0000

    feat: board game style upgrade

--- src/components/GameBoard.vue
+++ src/components/GameBoard.vue
@@ -1,18 +1,49 @@
 <script setup lang="ts">
 import GameCard from './GameCard.vue';
 import GameRowStats from './GameRowStats.vue';
+import GameColStats from './GameColStats.vue';
+import { matrix } from '@/assets/matrix';
+import GameRowColStats from './GameRowColStats.vue';
 
-import { matrix } from '@/assets/matrix'
+const getRowTotalBombs = (rowIndex: number): number => {
+  return matrix[rowIndex].reduce((count, value) => (value === 0 ? count + 1 : count), 0)
+}
+
+const getRowTotalPoints = (rowIndex: number): number => {
+  return matrix[rowIndex].reduce((acc, value) => acc + value, 0) || 0;  
+}
+
+const getColTotalBombs = (colIndex: number): number => {
+  let numberOfBombs: number = 0;
+  for (let i = 0; i < matrix.length; i++) {
+    if (matrix[i][colIndex] === 0) {
+      numberOfBombs++;
+    }
+  }
+  return numberOfBombs;
+}
+
+const getColTotalPoints = (colIndex: number): number => {
+  let points: number = 0;
+  for(let i = 0; i < matrix.length; i++) {
+    points += matrix[i][colIndex]
+  }
+  return points;
+}
+
+
 </script>
 
 <template>
-  <div class="bg-board-tealGreen h-96 w-96 grid grid-cols-6 p-2">
-    <GameCard :multiplier="matrix[0][0]" />
-    <GameCard :multiplier="matrix[0][1]" />
-    <GameCard :multiplier="matrix[0][2]" />
-    <GameCard :multiplier="matrix[0][3]" />
-    <GameCard :multiplier="matrix[0][4]" />
-    <GameRowStats :rowIndex="0" :row="matrix[0]" />
+  <div class="bg-board-tealGreen h-96 w-96 grid grid-cols-1 p-2">
+    <div class="grid grid-cols-6" v-for="(row, rowIndex) in matrix" :key="rowIndex">
+      <div v-for="(col, colIndex) in matrix" :key="colIndex">
+        <GameCard :multiplier="matrix[rowIndex][colIndex]" />
+        <GameRowColStats v-if="rowIndex === 4" :totalPoints="getColTotalPoints(colIndex)" :totalBombs="getColTotalBombs(colIndex)" class="mt-2"/>
+      </div>
+      <GameRowColStats :totalPoints="getRowTotalPoints(rowIndex)" :totalBombs="getRowTotalBombs(rowIndex)"/>
+    </div>
+
 
 
   </div>
---
+++ src/components/GameRowColStats.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import type { PropType } from 'vue';
+
+const props = defineProps({
+  totalPoints: Number,
+  totalBombs: Number,
+});
+</script>
+
+<template>
+  <div class="w-14 h-14 bg-red-500 border-solid border-4 border-gray-400">
+    <div class="flex flex-col items-center">
+      <div>
+        <span>{{ totalPoints }}</span>
+      </div>
+      <div>
+        <span>V: {{ totalBombs }}</span>
+      </div>
+    </div>
+  </div>
+</template>
